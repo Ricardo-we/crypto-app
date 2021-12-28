e1ccc34bae7cee9d7c137b6960b7069a7dbaa96d
Ricardo-we <totgamer75@gmail.com>
2021-12-28 13:32:57 -0600

    Changed routes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={<MainApp/>}/>
+        <Route path="/" element={<MainApp/>}/>
       </Routes>
     </BrowserRouter>
   );
